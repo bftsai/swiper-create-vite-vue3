d9f45f697692d2aa2f902a89ab5d1e600e481a86
bf_tsai <tsai19911026@gmail.com>
2023-12-26 01:39:58 +0800

    docs: ✏️ edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/swiper-vite-cli-vue3/'
-    : '/',
+  base: "/swiper-create-vite-vue3/",
   plugins: [
     vue(),
   ],
